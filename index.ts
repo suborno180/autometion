fc4fe5f20c5d4a5fb2fddf126b75d3a28036ff0d
suborno180 <64451255+suborno180@users.noreply.github.com>
2024-03-03 19:59:07 +0600

    update

--- index.ts
+++ index.ts
@@ -2,17 +2,29 @@ const puppeteer = require('puppeteer')
 
 
 const lauchedBrowser = async () => {
+
     const browserPromis = await puppeteer.launch({ headless: false })
         .then((browser) => {
             browser.pages()
-                .then((pages)=> {
-                    const mypage = pages[0];
-                    mypage.goto('https://translate.google.com/').then(()=>{
-                        console.log('reched');
-                        
-                    })
-                })
+                .then((pages) => {
+                    pages[0].goto('https://btebresulthub.vercel.app/results')
+                        .then(() => {
+                            pages[0].type("input[type=number]", `620340`)
+                            pages[0].click("button[type=submit]")
+                            // get the value from html p tag [p.relative]
 
+                            pages[0].evaluate(() => {
+                                const elements = document.querySelector("#parentView");
+                                return elements;
+                            }).then((elements) => {
+                                console.log(elements);
+                            })
+                        })
+
+
+                })
+        }).then(() => {
+            console.log("Browser closed")
         })
 }
 lauchedBrowser()
